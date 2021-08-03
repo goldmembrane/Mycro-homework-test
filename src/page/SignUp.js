79d9a9b1d95f89935e67f1a5ee397a5b6a40e73a
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-03 23:18:16 +0900

    create isEmail and isPassword functions and apply each input tag

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -1,16 +1,33 @@
 import React from 'react'
 
 const SignUp = () => {
+
+    // 이메일 체크 정규식
+    const isEmail = (e) => {
+        const emailRegExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+        
+        // 이메일 칸에 형식이 맞으면 true, 아니면 false를 반환한다.
+        return emailRegExp.test(e.target.value)
+    }
+
+    // 비밀번호 체크 정규식
+    const isPassword = (e) => {
+        const passwordRegExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,15}$/
+
+        // 비밀번호 칸에 8~15자이면 true, 아니면 false를 반환한다.
+        return passwordRegExp.test(e.target.value)
+    }
+
     return(
         <>
             <div className = 'sign-up-wrap'>
                 <div className = 'sign-up-email-box'>
                     <span>이메일</span>
-                    <input type = 'email' className = 'sign-up-email-input' />
+                    <input type = 'email' className = 'sign-up-email-input' onBlur = {isEmail}/>
                 </div>
                 <div className = 'sign-up-password-box'>
                     <span>비밀번호</span>
-                    <input type = 'password' className = 'sign-up-password-input'/>
+                    <input type = 'password' className = 'sign-up-password-input' onBlur = {isPassword}/>
                 </div>
                 <div className = 'sign-up-check-password-box'>
                     <span>비밀번호 확인</span>
