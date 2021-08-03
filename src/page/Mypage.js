aef115a589a1d5280581bc9afa08b0dd5d1458b3
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-03 17:45:43 +0900

    create Mypage.js file

---
+++ src/page/Mypage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Mypage = () => {
+    return(
+        <>
+            <div className = 'mypage-wrap'>
+                <div className = 'order-list'>
+                    order list
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Mypage
